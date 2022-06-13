6ca27944d50eee06a2e8c88fbef271ce014eadc3
hadi-pv <chumma1357@gmail.com>
2022-06-13 10:59:05 +0530

    commit 2

---
+++ components/homebottom.js
@@ -0,0 +1,69 @@
+import {Container,Col,Row, Form} from 'react-bootstrap'
+import styles from '../styles/homebottom.module.css'
+
+const sectors=['AgriTech','B2B','Consumer','Creator Community','Cyber Security','D2C','DeepTech','Dev Tools',
+               'E-Commerce','EdTech','FinTech','Gaming,Media & Ent','HealthTech','SaaS']
+
+const stages=['All','Early','Growth','Unicorn','Bootstrapped']
+
+const Homebottom=()=>{
+    return(
+        <div style={{'background-color':'gray'}}>
+        <Container className={styles.container}>
+            <Row>
+            <Col md={4} className={styles.leftcontainer}>
+                <div>
+                <Row className={styles.headings}>
+                    <center>Startups by sector</center>
+                </Row>
+                <Container>
+                    {
+                        sectors.map((sector)=>{
+                            return(
+                            <Row>
+                                <Container className={styles.sidebarrows}>
+                                    &emsp;{sector}
+                                </Container>
+                            </Row>    
+                            );
+                            
+                        })
+                    }
+                </Container>
+                </div>
+            </Col>
+
+            <Col  className={styles.rightcontainer}>
+                <div>
+                    <Row  className={styles.headings}>
+                        <center>Startups by Stage</center>
+                    </Row>
+                    <Row>
+                        {
+                            stages.map((stage)=>{
+                                return(
+                                    <Col>
+                                        <Container className={styles.rightbarcols}>
+                                            <center>{stage}</center>
+                                        </Container>
+                                    </Col>   
+                                );
+                               
+                            })
+                        }
+                    </Row>
+                    <Row className={styles.headings}>
+                        <span>Companies</span>
+                    </Row>
+                </div>
+            </Col>
+
+            </Row>
+            
+        </Container>   
+        </div>
+        
+    );
+}
+
+export default Homebottom;
--- pages/home.js
+++ pages/home.js
@@ -33,6 +33,7 @@ export default function Home({comp}) {
         totalComps={comp.length}
         paginate={paginate}
       />
+
        </div>
   
   );
