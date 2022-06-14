cea2fc270996f94f5b3144b6898b1d23ee1d9416
hadi-pv <chumma1357@gmail.com>
2022-06-14 13:50:55 +0530

    all_companies.json file

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,19 @@
 import {Homebottom,Homebottommobile} from "../components/homebottom";
 import AllCards from "../components/AllCards";
+import styles from '../styles/Home.module.css';
 
 const home=()=>{
     return(
-        <>
-            <div className="d-none d-md-block" style={{'backgroundColor':'#F5F5F5'}}>
+        <div style={{'backgroundColor':'#F5F5F5'}}>
+            <div className={styles.explore}><center>Explore</center></div>
+            <div className="d-none d-md-block">
                 <Homebottom/>
             </div>
-            <div className="d-xs-block d-md-none" style={{'backgroundColor':'#F5F5F5'}}>
+            <div className="d-xs-block d-md-none">
                 <Homebottommobile/>
             </div>
             
-        </>
+        </div>
     );
 }
 
