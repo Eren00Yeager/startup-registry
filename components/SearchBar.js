ed8ab8ea13e2d880153432f7a53df766a0d79454
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2022-06-14 13:59:17 +0530

    added search

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,9 +1,11 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 import { useState } from "react";
+import Image from "next/image";
+import search from '../pic/search.svg';
 import searchStyles from "../styles/searchbar.module.css";
 
 const SearchBar = ({ comp }) => {
-    const router = useRouter();
+  const router = useRouter();
   const [companyToSearch, setCompanySearch] = useState("");
   const changedSearch = (event) => {
     setCompanySearch(event.target.value);
@@ -16,35 +18,34 @@ const SearchBar = ({ comp }) => {
     router.push(`/${searchTerm}`);
   };
 
-  function isValid(nameinDb,enteredName){
-    enteredName =enteredName.toLowerCase();
-    nameinDb =  nameinDb.toLowerCase();
-    let m = enteredName.length
+  function isValid(nameinDb, enteredName) {
+    enteredName = enteredName.toLowerCase();
+    nameinDb = nameinDb.toLowerCase();
+    let m = enteredName.length;
     let n = nameinDb.length;
 
-    let allGood=0;
-    let j=0;
-    let i=0;
-    if(enteredName[i]!=nameinDb[j]){
-      if(m>2 && nameinDb.includes(enteredName) ){
+    let allGood = 0;
+    let j = 0;
+    let i = 0;
+    if (enteredName[i] != nameinDb[j]) {
+      if (m > 2 && nameinDb.includes(enteredName)) {
         return 1;
       }
-        return 0;   
+      return 0;
     }
     j++;
-    for(i=1;i<m;++i){
-        if(allGood>=3){
-            return 0;
-        }
-        if(enteredName[i]==nameinDb[j]){
-            allGood=0;
-            j++;
-        }
-        else{
-            allGood++;
-        }
+    for (i = 1; i < m; ++i) {
+      if (allGood >= 2) {
+        return 0;
+      }
+      if (enteredName[i] == nameinDb[j]) {
+        allGood = 0;
+        j++;
+      } else {
+        allGood++;
+      }
     }
-   
+
     return 1;
   }
 
@@ -54,33 +55,41 @@ const SearchBar = ({ comp }) => {
 
       <div className={searchStyles.searchcontainer}>
         <div className={searchStyles.searchInner}>
-          <input type="text" value={companyToSearch} maxLength="15" onChange={changedSearch} />
-          <button onClick={() => onSearch(companyToSearch)}> Search </button>
+        <div className={searchStyles.forImage}><Image src= {search}  ></Image></div>
+          <input
+            type="text"
+            value={companyToSearch}
+            maxLength="15"
+            placeholder="zolvit"
+            onChange={changedSearch}
+            className={searchStyles.forInput}
+          />
         </div>
         {/* implementing scroll view for this div required */}
-        <div className={searchStyles.dropdown}>
-          {comp
-            .filter((item) => {
-              const searchTerm = companyToSearch.toLowerCase();
-              const fullName = item.Name.toLowerCase();
+        <div className={searchStyles.idrop}>
+          <div className={searchStyles.dropdown}>
+            {comp
+              .filter((item) => {
+                const searchTerm = companyToSearch.toLowerCase();
+                const fullName = item.Name.toLowerCase();
 
-              return (
-                searchTerm &&
-                (fullName.startsWith(searchTerm)  ||
-                isValid(fullName,searchTerm)) &&
-                fullName !== searchTerm
-              );
-            })
-            .slice(0, 10)
-            .map((item) => (
-              <div
-                onClick={() => onSearch(item.Name)}
-                className={searchStyles.dropdownRow}
-                key={item.Name}
-              >
-                {item.Name}
-              </div>
-            ))}
+                return (
+                  // searchTerm &&
+                  (fullName.startsWith(searchTerm) ||
+                    isValid(fullName, searchTerm)) &&
+                  fullName !== searchTerm
+                );
+              })
+              .map((item) => (
+                <div
+                  onClick={() => onSearch(item.Name)}
+                  className={searchStyles.dropdownRow}
+                  key={item.Name}
+                >
+                  {item.Name}
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
