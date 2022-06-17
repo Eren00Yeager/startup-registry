9f1ec60e4bccbc513d1234ca5ad3059cc343915c
unknown <vijay05086@gmail.com>
2022-06-17 17:40:59 +0530

    homeTop

--- components/hometop.js
+++ components/hometop.js
@@ -7,12 +7,18 @@ import g3 from "../images/g3.svg";
 import p1 from "../images/p1.svg";
 import p2 from "../images/p2.svg";
 import p3 from "../images/p3.svg";
+import plus from "../images/plus.svg";
+import minus from "../images/minus.svg";
 
 function Hometop() {
     const [isActive, setActive] = useState("false");
+    const [Text, setText] = useState("Out");
     const ToggleClass = () => {
         setActive(!isActive);
+        setText(isActive ? "In" : "Out");
     };
+
+    
 
     return (
 
@@ -21,59 +27,67 @@ function Hometop() {
                 <div className={styles.g}>
                     <div>
                         <div className={styles.d1}>Total Unicorns</div>
-                        <div style={{ fontSize: "49px" }} className={styles.check} >100<span style={{ fontSize: "20px" }}>+1</span></div>
+                        <div className={styles.d1font} >100<span className={styles.plusone}>+1</span></div>
                         <Image src={g1} />
                     </div>
-                    <span className={isActive ? styles.hide : ""}>
-                        <div>
-                            <div className={styles.d1}>Total Unicorns</div>
-                            <div style={{ fontSize: "49px" }} className={styles.check} >100<span style={{ fontSize: "20px" }}>+1</span></div>
-                            <Image src={g2} />
-                        </div>
-                        <div>
-                            <div className={styles.d1}>Total Unicorns</div>
-                            <div style={{ fontSize: "49px" }} className={styles.check} >100<span style={{ fontSize: "20px" }}>+1</span></div>
-                            <Image src={g3} />
-                        </div>
-                        <div>
-                            <div className={styles.d1}>Total Unicorns</div>
-                            <div style={{ fontSize: "49px" }} className={styles.check} >100<span style={{ fontSize: "20px" }}>+1</span></div>
-                            <Image src={g1} />
-                        </div>
-                    </span>
+                    {/* <div className={isActive ? styles.hide : styles.duration}> */}
+                    <div className={isActive ? styles.hide : styles.duration}>
+                        <div className={styles.d1}>Total Unicorns</div>
+                        <div className={styles.d1font} >100<span className={styles.plusone}>+1</span></div>
+                        <Image src={g2} />
+                    </div>
+                    <div className={isActive ? styles.hide : styles.duration}>
+                        <div className={styles.d1}>Total Unicorns</div>
+                        <div className={styles.d1font} >100<span className={styles.plusone}>+1</span></div>
+                        <Image src={g3} />
+                    </div>
+                    <div className={isActive ? styles.hide : styles.duration}>
+                        <div className={styles.d1}>Total Unicorns</div>
+                        <div className={styles.d1font} >100<span className={styles.plusone}>+1</span></div>
+                        <Image src={g1} />
+                    </div>
+                    {/* </div> */}
                 </div>
                 <div className={styles.allBoxes}>
                     <div className={styles.twoBoxes}>
                         <div className={isActive ? styles.halfb1 : styles.b1}>
                             <div className={styles.boxheading}>Funding Tracker</div>
                             <div className={styles.boxcontent}>
-                                <div className={styles.svgs}><Image src={p1} /> <div style={{fontWeight: "500"}}>Pepper Content raised $14.3 M</div></div>
+                                <div className={styles.svgs}><Image src={p1} /> <div style={{ fontWeight: "500" }}>Pepper Content raised $14.3 M</div></div>
                                 <div className={styles.svgs}><Image src={p2} /> <div>Samudai web3 startup raised $2.5 M</div></div>
                                 <div className={styles.svgs}><Image src={p3} /> <div>Fintech startup Jiraaf raised $7.5M</div></div>
                             </div>
                         </div>
-                        <div className={isActive ? styles.halfb1 : styles.b1}>
+                        <div className={isActive ? `${styles.halfb1} ${styles.hide12}` : styles.b1}>
                             <div className={styles.boxheading}>Startup of the Week</div>
-                            {/* <div>
-                                Startup of the Week
-                                <br />
-                                Startup of the Week
-                                <br />
-                                Startup of the Week
-                            </div> */}
+                            <div className={styles.boxcontent}>
+                                <div className={styles.svgs}><Image src={p1} /> <div style={{ fontWeight: "500" }}>Pepper Content raised $14.3 M</div></div>
+                                <div className={styles.svgs}><Image src={p2} /> <div>Samudai web3 startup raised $2.5 M</div></div>
+                                <div className={styles.svgs}><Image src={p3} /> <div>Fintech startup Jiraaf raised $7.5M</div></div>
+                            </div>
                         </div>
                     </div>
                     <div className={isActive ? styles.hide : styles.twoBoxes}>
                         <div className={styles.b2}>
                             <div className={styles.boxheading}>Startups in News</div>
+                            <div className={styles.boxcontent}>
+                                <div className={styles.svgs}><Image src={p1} /> <div style={{ fontWeight: "500" }}>Pepper Content raised $14.3 M</div></div>
+                                <div className={styles.svgs}><Image src={p2} /> <div>Samudai web3 startup raised $2.5 M</div></div>
+                                <div className={styles.svgs}><Image src={p3} /> <div>Fintech startup Jiraaf raised $7.5M</div></div>
+                            </div>
                         </div>
                         <div className={styles.b2}>
                             <div className={styles.boxheading}>Concepts you  may or may not know</div>
+                            <div className={styles.boxcontent}>
+                                <div className={styles.svgs}><Image src={p1} /> <div style={{ fontWeight: "500" }}>Pepper Content raised $14.3 M</div></div>
+                                <div className={styles.svgs}><Image src={p2} /> <div>Samudai web3 startup raised $2.5 M</div></div>
+                                <div className={styles.svgs}><Image src={p3} /> <div>Fintech startup Jiraaf raised $7.5M</div></div>
+                            </div>
                         </div>
                     </div>
-                    <button onClick={ToggleClass}>Click Me!</button>
                 </div>
             </div>
+            <button className={styles.check} onClick={ToggleClass}><div className={styles.check2}>Nerd {Text}! <div><Image src={isActive ? plus : minus} /></div></div></button>
         </div>
 
 
