3333f0e7318ef6ab7eb33da5b26951c758f0fdd9
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2022-07-11 18:13:16 +0530

    works

--- components/indvCard.js
+++ components/indvCard.js
@@ -1,40 +1,37 @@
 import styles from "../styles/card.module.css";
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
-const IndvCard = ({ data,setSinglecomp }) => {
+const IndvCard = ({ data, setSinglecomp }) => {
+  let str = data.Logo;
+  str = str.slice(32, 65);
 
-   let str = data.Logo;
-   str = str.slice(32, 65);
+  let str2 = data.Shortbrief;
+  let s = "";
+  if (str2) {
+    const iterator = str2[Symbol.iterator]();
+    let theChar = iterator.next();
 
-   let str2 = data.Shortbrief;
-   let s = "";
-   if(str2){
-   const iterator = str2[Symbol.iterator]();
-   let theChar = iterator.next();
-  
-   while (!theChar.done && (theChar.value !== '.' || (s.length < 10))) {
-       s = s + theChar.value
-       theChar = iterator.next();
-   }}
-  
+    while (!theChar.done && (theChar.value !== "." || s.length < 10)) {
+      s = s + theChar.value;
+      theChar = iterator.next();
+    }
+  }
 
   return (
-    <div onClick={()=>setSinglecomp(data)}>
-     <button className={styles.forButton}>
-      <div className={styles.colFlex2}>
-      <Image
-    className={styles.forImage}
-      src={`https://drive.google.com/uc?export=view&id=${str}`}
-      alt="Picture of the author"
-      width={400}
-      height={400}
-    />
-      
-        <div className={styles.Valuation}>{data.Valuation}</div>
-        <div className={styles.headPtext}>{data.Name}</div>
-        <p className={styles.pText}>{`${s}`}</p>
-      </div>
+    <div onClick={() => setSinglecomp(data)}>
+      <button className={styles.forButton}>
+        <div className={styles.colFlex2}>
+          <div>
+            <img
+              className={styles.forImage}
+              src={`https://drive.google.com/uc?export=view&id=${str}`}
+            />
+          </div>
+          <div className={styles.Valuation}>{data.Valuation}</div>
+          <div className={styles.headPtext}>{data.Name}</div>
+          <p className={styles.pText}>{`${s}`}</p>
+        </div>
       </button>
     </div>
   );
